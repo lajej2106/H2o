a10acc28ce18f725df4df6508732e11408dfd8b9
jeremy <jeremy.cotigny@gmail.com>
2022-10-19 22:17:09 +0200

    Chore(init): init angular project

--- packages/back/src/main/java/com/H2o/H2oApplication.java
+++ packages/back/src/main/java/com/H2o/H2oApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class H2oApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(H2oApplication.class, args);
     }
